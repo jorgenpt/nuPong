d97fab6e68212443dc4f9751c7ed7dedf49d1bfe
Jørgen P. Tjernø <jtjerno@mylookout.com>
2012-10-14 11:12:19 -0700

    Initial Commit

---
+++ nuPong/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  nuPong
+//
+//  Created by Jørgen Tjernø on 10/14/12.
+//  Copyright (c) 2012 bitSpatter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
